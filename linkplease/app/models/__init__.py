70017418a9ce3f27f24c97c40647a3ef9fbe6a0f
Shantanu Paliwal <shantanupaliwal560@gmail.com>
2026-08-18 21:35:49 +0530

    fixed some errors

--- linkplease/app/models/__init__.py
+++ linkplease/app/models/__init__.py
@@ -2,12 +2,12 @@ from app.models.rule import Rule
 from app.models.event import Event
 from app.models.comment import Comment
 from app.models.dm_job import DMJob
-from app.models.blocked_duplicate import BlockedDuplicate
+from app.models.blocked_duplicate import BlockedDuplicateEvent
 
 __all__ = [
     "Rule",
     "Event",
     "Comment",
     "DMJob",
-    "BlockedDuplicate",
+    "BlockedDuplicateEvent",
 ]
